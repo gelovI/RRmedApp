5e350861500cb3ebd5aa5c5f12e016d3fbf31585
IvanG <ivan.gelov@gmx.de>
2025-04-18 16:02:19 +0200

    Schnellanalyse

--- app/src/main/java/com/example/bloodpressureapp/data/Therapy.kt
+++ app/src/main/java/com/example/bloodpressureapp/data/Therapy.kt
@@ -2,6 +2,7 @@ package com.example.bloodpressureapp.data
 
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
 @Entity(
@@ -11,7 +12,8 @@ import androidx.room.PrimaryKey
         parentColumns = ["id"],
         childColumns = ["userId"],
         onDelete = ForeignKey.CASCADE
-    )]
+    )],
+    indices = [Index(value = ["userId"])]
 )
 data class Therapy(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
